83f265ac670117a6154e92cb45b458df7668583c
Siddharth-singh2111 <siddharth.s23@iiits.in>
2025-07-09 16:12:50 +0530

    🔧 Moved Firebase credentials to .env.local and updated config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBlkypDElllf_zXCMFiHCGNO1bX2XaGHto",
-  authDomain: "personalized-dashboard-c2503.firebaseapp.com",
-  projectId: "personalized-dashboard-c2503",
-  appId: "1:436550627038:web:86c71351cc086533425c4f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
